9ce52bd9a03c5df6f5f04778108602278f30568e
Bailey Kursar <bkursar@gmail.com>
2017-06-13 10:58:38 +0100

    Wrote a test to check if an account has a balance of 0 when initialized and made it pass

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,12 @@ describe Account do
   before(:each) do
     @account = Account.new
   end
+
   it 'an account can be created' do
     expect(@account).to be_an_instance_of(Account)
+  end
+
+  it 'an account has a balance' do
+    expect(@account.balance).to eq 0
   end
 end
